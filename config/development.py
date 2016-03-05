a68bb0ebbde93be87b241b389a791c464f76c877
endyul <zendyul@gmail.com>
2016-03-06 00:44:58 +0800

    update ignore

--- config/development.py
+++
@@ -1,5 +0,0 @@
-from config.default import *
-
-DEBUG = True
-MAIL_DEFAULT_SENDER = 'zxxu@ir.hit.edu.cn'
-SQLALCHEMY_DATABASE_URI = 'sqlite://///Users/Oliver/Development/moss/moss.db'
--- config/production.py
+++
@@ -1,4 +0,0 @@
-from config.default import *
-
-DEBUG = False
-MAIL_DEFAULT_SENDER = 'zxxu@ir.hit.edu.cn'
