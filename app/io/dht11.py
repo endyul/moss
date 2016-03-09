7eebdddc5fb177b706df52b0d152f9a937d1c4c8
endyul <zendyul@gmail.com>
2016-03-10 00:55:56 +0800

    update

--- app/io/dht11.py
+++ app/io/dht11.py
@@ -1,14 +1,11 @@
 import serial
 import sys
 
-g_port = '/dev/cu.usbserial-AH01A8MP'
-g_baundrate = 9600
 
 class DHT11(object):
-    def __init__(self):
-        global g_port, g_baundrate
-        self.port = g_port
-        self.baundrate = g_baundrate
+    def __init__(self, port='/dev/cu.usbserial-AH01A8MP', baundrate=9600):
+        self.port = port
+        self.baundrate = baundrate
         self.ser = serial.Serial(self.port, self.baundrate)
 
     def read(self):
